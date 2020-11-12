9fc8d650f3437a7355f800b96ad9b560e3c6cbc0
ADamian17 <adonisdesuvikdo@gmail.com>
2020-11-12 15:48:04 -0800

    fixed register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -60,108 +60,106 @@ class Register extends Component {
 
         return (
             <LoginRegisterContainer colZise={6} >
-
-                <div className="card p-3 shadow">
+               
                     
-                    { errMessage ?  
-                        <ErrorBanner message={ errMessage.message } />
-                        : '' 
-                    }
+                { errMessage ?  
+                    <ErrorBanner message={ errMessage.message } />
+                    : '' 
+                }
 
-                    <div className="row">
+                <div className="row">
 
-                        <div className="form-group col p-2">
+                    <div className="form-group col p-2">
 
-                            <header className="text-center">
-                                create a new account
-                            </header>
+                        <header className="text-center">
+                            create a new account
+                        </header>
                             
+                    </div>
+
+                </div>
+                    
+                <form>
+
+                    {/* row 1 */}
+                    <div className="form-row">
+
+                        <div className="form-group col">
+                            <input
+                                className="form-control"
+                                placeholder="First Name"
+                                name="first_name"
+                                onChange={this.handleChange}
+                                required />
+                        </div>
+
+                        <div className="form-group col">
+                            <input
+                                className="form-control"
+                                placeholder="Last Name"
+                                type="text"
+                                name="last_name"
+                                onChange={this.handleChange}
+                                required/>
+                        </div>
+
+                        <div className="form-group col">
+                            <input
+                                className="form-control"
+                                placeholder="Profession"
+                                type="text"
+                                name="profession"
+                                onChange={this.handleChange}
+                                required/>
+                        </div> 
+
+                    </div>
+
+                    {/* row 2 */}
+                    <div className="form-group">
+                        <input
+                            className="form-control"
+                            placeholder="E-mail address"
+                            name="email"
+                            onChange={this.handleChange}
+                            required />
+                            
+                    </div>
+
+                    {/* row 3  */}
+                    <div className="form-row">
+                            
+                        <div className="form-group col">
+                            <input
+                                className="form-control"
+                                placeholder="Password"
+                                type="password"
+                                name="password"
+                                onChange={this.handleChange}
+                                required />
+
+                        </div>
+
+                        <div className="form-group col">
+                            <input
+                                className="form-control"
+                                placeholder="Confirm Password"
+                                type="password"
+                                name="password2"
+                                onChange={this.handleChange}
+                                required/>
                         </div>
 
                     </div>
-                    
-                    <form>
 
-                        {/* row 1 */}
-                        <div className="form-row">
-
-                            <div className="form-group col">
-                                <input
-                                    className="form-control"
-                                    placeholder="First Name"
-                                    name="first_name"
-                                    onChange={this.handleChange}
-                                    required />
-                            </div>
-
-                            <div className="form-group col">
-                                <input
-                                    className="form-control"
-                                    placeholder="Last Name"
-                                    type="text"
-                                    name="last_name"
-                                    onChange={this.handleChange}
-                                    required/>
-                            </div>
-
-                            <div className="form-group col">
-                                <input
-                                    className="form-control"
-                                    placeholder="Profession"
-                                    type="text"
-                                    name="profession"
-                                    onChange={this.handleChange}
-                                    required/>
-                            </div> 
-
-                        </div>
-
-                        {/* row 2 */}
-                        <div className="form-group">
-                            <input
-                                className="form-control"
-                                placeholder="E-mail address"
-                                name="email"
-                                onChange={this.handleChange}
-                                required />
-                            
-                        </div>
-
-                        {/* row 3  */}
-                        <div className="form-row">
-                            
-                            <div className="form-group col">
-                                <input
-                                    className="form-control"
-                                    placeholder="Password"
-                                    type="password"
-                                    name="password"
-                                    onChange={this.handleChange}
-                                    required />
-
-                            </div>
-
-                            <div className="form-group col">
-                                <input
-                                    className="form-control"
-                                    placeholder="Confirm Password"
-                                    type="password"
-                                    name="password2"
-                                    onChange={this.handleChange}
-                                    required/>
-                            </div>
-
-                        </div>
-
-                        <button type="submit" className="btn btn-primary btn-block" onClick={this.handleRegister}>
-                            Register
-                        </button>
+                    <button type="submit" className="btn btn-primary btn-block" onClick={this.handleRegister}>
+                        Register
+                    </button>
     
-                    </form>
+                </form>
 
-                    <Message message="Already have an Account?" url="/login" title="Login" />
-
-                </div>
+                <Message message="Already have an Account?" url="/login" title="Login" />
+            
 
             </LoginRegisterContainer>
         );
